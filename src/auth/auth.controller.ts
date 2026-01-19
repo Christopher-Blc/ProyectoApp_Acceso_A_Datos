3627d8109fa300d95b38d5733e9d5d3a9c91880d
Javier <javgistal@alu.edu.gva.es>
2026-01-19 17:11:44 +0100

    haciendo cambios en el refresh token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 import { AuthService } from './auth.service';
@@ -7,41 +6,24 @@ import { AuthGuard } from './auth.guard';
 import { RefreshDto } from "./dto/refresh.dto";
 
 
-@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  @ApiOperation({ summary: 'Registrar un nuevo usuario' })
-  @ApiResponse({ status: 201, description: 'Usuario registrado exitosamente.' })
-  @ApiResponse({ status: 409, description: 'El email ya está registrado.' })
   register(@Body() dto: RegisterDto) {
     return this.authService.register(dto);
   }
 
   @Post('login')
-  @ApiOperation({ summary: 'Iniciar sesión y obtener Token JWT' })
-  @ApiResponse({ 
-    status: 200, 
-    description: 'Login exitoso. Retorna el token de acceso.',
-    schema: {
-      type: 'object',
-      properties: {
-        access_token: { type: 'string', example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...' },
-        user: { type: 'object' },
-      }
-    }
-  })
-  @ApiResponse({ status: 401, description: 'Credenciales incorrectas.' })
   login(@Body() dto: LoginDto) {
     return this.authService.login(dto);
   }
 
   @Post("refresh")
-refresh(@Body() dto: RefreshDto) {
-  return this.authService.refresh(dto.refresh_token);
-}
+  refresh(@Body() dto: RefreshDto) {
+    return this.authService.refresh(dto.refresh_token);
+  }
 
 
 
