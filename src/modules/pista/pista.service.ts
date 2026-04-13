8f84999e74bc24ea0a3d9ef0e35ec7d3b872ce42
Christopher-Blc <chris.bolocan@gmail.com>
2026-04-13 15:42:29 +0200

    arreglo fallo en relaciones de pista

--- src/modules/pista/pista.service.ts
+++ src/modules/pista/pista.service.ts
@@ -9,18 +9,18 @@ export class PistaService {
 
     constructor(
         @InjectRepository(Pista)
-        private readonly pistaRepo: Repository<Pista>, // `userRepo` es el acceso a todas las operaciones de la tabla User
+        private readonly pistaRepo: Repository<Pista>, // `pistaRepo` es el acceso a todas las operaciones de la tabla Pista
       ) {}
 
       async findAll(): Promise<Pista[]>{
 
-        return this.pistaRepo.find({relations: ['reservas', 'comentarios', 'horarios_pista', 'instalacion']});
+        return this.pistaRepo.find({relations: ['reservas', 'resenya', 'horarios_pista', 'instalacion']});
       } 
 
       async findOne(pista_id: number ): Promise<Pista>{
 
         const pista = await this.pistaRepo.findOne({where: {pista_id: pista_id},
-                                            relations: ['reservas', 'comentarios', 'horarios_pista', 'instalacion']});
+                                            relations: ['reservas', 'resenya', 'horarios_pista', 'instalacion']});
         if (!pista){
             throw new NotFoundException(`Pista ${pista_id} no encontrada`); // Lanzamos un error si no se encuentra la pista
         }
