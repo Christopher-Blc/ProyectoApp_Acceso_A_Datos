b3dd741b68891ad33ea3659c6221db731d8beb04
Christopher-Blc <chris.bolocan@gmail.com>
2026-04-27 10:27:30 +0200

    fix error de importacion / export de modulo de reserva y pista

--- src/modules/pista/pista.module.ts
+++ src/modules/pista/pista.module.ts
@@ -3,9 +3,10 @@ import { PistaService } from './pista.service';
 import { PistaController } from './pista.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Pista } from './entities/pista.entity';
+import { ReservaModule } from '../reserva/reserva.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Pista])],
+  imports: [TypeOrmModule.forFeature([Pista]), ReservaModule],
   providers: [PistaService],
   controllers: [PistaController]
 })
